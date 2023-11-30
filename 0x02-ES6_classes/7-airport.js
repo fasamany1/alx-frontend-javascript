51531639b1080602ea0cd5648c4b5f029c7b3ccf
Forster Asamany <asamanyforster@yahoo.com>
2023-11-30 10:09:39 -0500

    Update a class named Airport

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -3,6 +3,7 @@ export default class Airport {
     this.name = name;
     this.code = code;
   }
+  
   // get-gets the name or code and set sets the name or code.
   get name() {
     return this._name;
