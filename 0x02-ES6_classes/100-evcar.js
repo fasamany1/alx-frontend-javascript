e924766b1fbe687f032d0e4dba0ee6e6a2bde593
Forster Asamany <asamanyforster@yahoo.com>
2023-12-01 02:15:49 -0500

    Update a class named EVCar

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -13,8 +13,7 @@ export default class EVCar extends Car {
   set range(value) {
     this._range = value;
   }
-  // returns instance of Car instead
-  cloneCar() {
+  cloneCar() {// returns instance of Car instead
     const Species = super.constructor[Symbol.species];
 
     return new Species();
