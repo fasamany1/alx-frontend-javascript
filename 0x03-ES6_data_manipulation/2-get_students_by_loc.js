97743309f59d7ce7aa24131adac03dd045250c84
Forster Asamany <asamanyforster@yahoo.com>
2023-12-06 00:13:03 -0500

    Return an array of objects

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,7 @@
+// Retrieves students in a given location.
+export default function getStudentsByLocation(students, city) {
+  if (students instanceof Array) {
+    return students.filter((student) => student.location === city);
+  }
+  return [];
+}
